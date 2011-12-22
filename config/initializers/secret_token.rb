35a2040ad15f6b76d784f1a5867f70234d9bbff0
Sum Breh <sumdude@sumwhere.net>
2011-12-22 12:02:04 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NubeAp::Application.config.secret_token = '0076e3d052cc1b9fde230c8d7729ebe69bfb965c70b1321e728f8654ae82718b21960dbf2fb6dd04ffe599a70f9930cc614661b925f8a0319f27225b7f5f4ef7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NubeAp::Application.config.session_store :cookie_store, key: '_nube_ap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NubeAp::Application.config.session_store :active_record_store
